67a4cbc2df8be5294e80f3457b483d115f35f057
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-28 00:11:12 -0300

    Email not repeated in client update

--- src/main/java/com/DTO/ClienteDTO.java
+++ src/main/java/com/DTO/ClienteDTO.java
@@ -8,8 +8,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.domain.Cliente;
+import com.services.validation.ClienteUpdate;
 
-@SuppressWarnings("deprecation")
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
