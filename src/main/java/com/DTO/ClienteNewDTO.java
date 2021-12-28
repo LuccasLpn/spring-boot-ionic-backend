22bba58e13144872d86cc3fce4087dac0f738406
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-27 23:16:20 -0300

    Validator Customizing

--- src/main/java/com/DTO/ClienteNewDTO.java
+++ src/main/java/com/DTO/ClienteNewDTO.java
@@ -2,21 +2,52 @@ package com.DTO;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@SuppressWarnings("deprecation")
+	@NotEmpty(message = "Prenchimento Obrigatorio")
+	@Length(min = 5, max = 120, message = "O tamnho deve ser entre 5 e 120 caracteres")
 	private String name;
+	
+	@SuppressWarnings("deprecation")
+	@NotEmpty(message = "Prenchimento Obrigatorio")
+	@Length(min = 5, max = 120, message = "O tamnho deve ser entre 5 e 120 caracteres")
 	private String email;
+	
+	@SuppressWarnings("deprecation")
+	@NotEmpty(message = "Prenchimento Obrigatorio")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@SuppressWarnings("deprecation")
+	@NotEmpty(message = "Prenchimento Obrigatorio")
 	private String logradouro;
+	
+	@SuppressWarnings("deprecation")
+	@NotEmpty(message = "Prenchimento Obrigatorio")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@SuppressWarnings("deprecation")
+	@NotEmpty(message = "Prenchimento Obrigatorio")
 	private String cep;
 	
+	@NotEmpty(message = "Prenchimento Obrigatorio")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
