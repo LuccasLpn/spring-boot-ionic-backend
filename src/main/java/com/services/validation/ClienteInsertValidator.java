0deb6aa31669f7019f647ee0fe39fd9072887bc9
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-27 23:55:57 -0300

    do not insert and repeated email

--- src/main/java/com/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.DTO.ClienteNewDTO;
+import com.domain.Cliente;
 import com.domain.enums.TipoCliente;
+import com.repository.ClienteRepository;
 import com.resources.exception.FieldMessage;
 import com.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -25,6 +32,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && ! BR.isValidCNPJ(objDto.getCpfOuCnpj())){
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("Email", "Email já existente"));
 		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
