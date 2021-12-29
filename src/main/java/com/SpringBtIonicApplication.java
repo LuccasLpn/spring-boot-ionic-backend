b2c8e92ad952357a6da7d62fcb4c5a53357a5dd9
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-29 14:24:58 -0300

    Implements ToString

--- src/main/java/com/SpringBtIonicApplication.java
+++ src/main/java/com/SpringBtIonicApplication.java
@@ -1,6 +1,5 @@
 package com;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ public class SpringBtIonicApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringBtIonicApplication.class, args);
-		
-		
-}
+
+	}
 }
