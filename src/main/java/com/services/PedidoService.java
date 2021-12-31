59d1872c65d453b225caf54edd20d2a23ab34f79
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-31 13:38:42 -0300

    Content restriction: customer only retrieves their orders

--- src/main/java/com/services/PedidoService.java
+++ src/main/java/com/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.domain.Cliente;
 import com.domain.ItemPedido;
 import com.domain.PagamentoComBoleto;
 import com.domain.Pedido;
@@ -14,6 +18,8 @@ import com.domain.enums.EstadoPagamento;
 import com.repository.ItemPedidoRepository;
 import com.repository.PagamentoRepository;
 import com.repository.PedidoRepository;
+import com.security.UserSS;
+import com.services.exceptions.AuthorizationException;
 import com.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,6 +76,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null ) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.findById(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 
 }
