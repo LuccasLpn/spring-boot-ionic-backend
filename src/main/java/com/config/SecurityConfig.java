efa7bcc04619a3df69d32990dae1021058762c1a
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-31 11:59:22 -0300

    correcting configure method

--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -55,11 +55,12 @@ public class  SecurityConfig extends WebSecurityConfigurerAdapter{
 		}
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll().antMatchers(HttpMethod.POST,PUBLIC_MATCHERS_POST)
-		.permitAll().anyRequest().authenticated();
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
