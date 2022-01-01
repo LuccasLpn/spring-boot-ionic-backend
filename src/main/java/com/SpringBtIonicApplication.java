f8fa2a4467ca611534de5b45c1f3294895f91349
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-01 14:16:13 -0300

    Amazon bucket

--- src/main/java/com/SpringBtIonicApplication.java
+++ src/main/java/com/SpringBtIonicApplication.java
@@ -1,14 +1,28 @@
 package com;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.services.S3Service;
+
 @SpringBootApplication
-public class SpringBtIonicApplication {
+public class SpringBtIonicApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service service;
+	
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringBtIonicApplication.class, args);
+		
+	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		service.uploadFile("C:\\setup.jpeg");
+		
 	}
 }
